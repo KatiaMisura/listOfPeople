88c0188fd7ec9748de90b5dbbc000d1d66474a29
PetarTrutanic <petar.trutanic@gmail.com>
2023-03-28 14:19:23 +0200

    Added edit template

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -11,14 +11,28 @@ function Details() {
     const id = routeParams.id;
     const osoba = findById(id);
     setContact(osoba);
-  }, []);
+  }, [findById, routeParams.id]);
+
+  const handleNameChange = (event) => {
+    const newName = event.target.value;
+    const newContact = {
+      ...contact,
+      name: newName
+    }
+    setContact(newContact);
+  }
+
+  const handleSave = () => {
+    // Save changes
+  }
 
   return (
     <div className='user-details'>
       User Details Component
       <div>
-        {contact ? contact.name : ""}
+        <input type="text" defaultValue={contact ? contact.name : ""} onChange={handleNameChange} />
       </div>
+      <button onClick={handleSave}>Save</button>
     </div>
   );
 }
