5884b6cadd2e7843c11dc7cfc74e5d9bb46ac2ab
PetarTrutanic <petar.trutanic@gmail.com>
2023-03-28 14:13:21 +0200

    Added server connection

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,68 +1,71 @@
 import { createContext, useState, useEffect } from "react";
-import { contacts } from "../data/contacts";
+import axios from "axios";
 
 export const AppContext = createContext();
 
 export function AppContextProvider(Component) {
-    return function Context(props) {
-        const [contactList, setContactList] = useState(contacts);
-        const [displayedContacts, setDisplayedContacts] = useState(contactList);
-        const [categoryFilter, setCategoryFilter] = useState("");
-        const [cityFilter, setCityFilter] = useState("");
-        const [yearFilter, setYearFilter] = useState("");
+  return function Context(props) {
+    const [contactList, setContactList] = useState([]);
+    const [displayedContacts, setDisplayedContacts] = useState(contactList);
+    const [categoryFilter, setCategoryFilter] = useState("");
+    const [cityFilter, setCityFilter] = useState("");
+    const [yearFilter, setYearFilter] = useState("");
 
-        // https://server-app-git-main-ptrutanic.vercel.app/api/contacts
+    useEffect(() => {
+      axios
+        .get("https://server-app-git-main-ptrutanic.vercel.app/api/contacts")
+        .then((result) => {
+          setContactList(result.data.contacts);
+        });
+    }, []);
 
-        useEffect(() => {
-            const newContactList = contactList.filter((contact) => {
-                const isCategoryShown =
-                    categoryFilter === "" ||
-                    categoryFilter === contact.category;
-                const isCityShown =
-                    cityFilter === "" || cityFilter === contact.city;
+    useEffect(() => {
+      const newContactList = contactList.filter((contact) => {
+        const isCategoryShown =
+          categoryFilter === "" || categoryFilter === contact.category;
+        const isCityShown = cityFilter === "" || cityFilter === contact.city;
 
-                const yearOfBirth = new Date(contact.dateOfBirth).getFullYear();
-                const isYearShown =
-                    yearFilter === "" || yearOfBirth < yearFilter;
+        const yearOfBirth = new Date(contact.dateOfBirth).getFullYear();
+        const isYearShown = yearFilter === "" || yearOfBirth < yearFilter;
 
-                return isCategoryShown && isCityShown && isYearShown;
-            });
-            setDisplayedContacts(newContactList);
-        }, [contactList, categoryFilter, cityFilter, yearFilter]);
+        return isCategoryShown && isCityShown && isYearShown;
+      });
+      setDisplayedContacts(newContactList);
+    }, [contactList, categoryFilter, cityFilter, yearFilter]);
 
-        const deleteContact = (contactId) => {
-            const newList = contactList.filter((contact) => {
-                return contact.id !== contactId;
-            });
-            setContactList(newList);
-        };
-
-        const createContact = (contact) => {
-            const newList = [...contactList, contact];
-            setContactList(newList);
-        };
-
-        const findById = (id) => {
-            const contact = contactList.find((contact) => {
-                return contact.id == id;
-            });
-            return contact;
-        };
-
-        return (
-            <AppContext.Provider
-                value={{
-                    displayedContacts,
-                    createContact,
-                    deleteContact,
-                    setCategoryFilter,
-                    setCityFilter,
-                    setYearFilter,
-                    findById,
-                }}
-            >
-                <Component {...props} />
-            </AppContext.Provider>
-        );
+    const deleteContact = (contactId) => {
+      const newList = contactList.filter((contact) => {
+        return contact.id !== contactId;
+      });
+      setContactList(newList);
     };
+
+    const createContact = (contact) => {
+      const newList = [...contactList, contact];
+      setContactList(newList);
+    };
+
+    const findById = (id) => {
+      const contact = contactList.find((contact) => {
+        return contact.id.toString() === id.toString();
+      });
+      return contact;
+    };
+
+    return (
+      <AppContext.Provider
+        value={{
+          displayedContacts,
+          createContact,
+          deleteContact,
+          setCategoryFilter,
+          setCityFilter,
+          setYearFilter,
+          findById,
+        }}
+      >
+        <Component {...props} />
+      </AppContext.Provider>
+    );
+  };
 }
--- src/data/contacts.js
+++ src/data/contacts.js
@@ -25,50 +25,50 @@ export function Contact(
   this.id = getRandomNumber();
 }
 
-export const contacts = [
-  new Contact(
-    "Ivo",
-    "ivić",
-    "1995-10-16",
-    "156181554",
-    "Moja Kuca 1",
-    "Split",
-    "red"
-  ),
-  new Contact(
-    "Ante",
-    "Lukač",
-    "1989-03-23",
-    "15132131554",
-    "Moja Kuca 2",
-    "Split",
-    "green"
-  ),
-  new Contact(
-    "Josip",
-    "Horvat",
-    "1968-08-30",
-    "153231554",
-    "Moja Kuca 3",
-    "Zagreb",
-    "green"
-  ),
-  new Contact(
-    "Luka",
-    "Horvat",
-    "1993-04-05",
-    "15313554",
-    "Moja Kuca 4",
-    "Osjek",
-    "red"
-  ),
-  new Contact(
-    "mate",
-    "Horvat",
-    "2001-03-07",
-    "1561232554",
-    "Moja Kuca 5",
-    "Split",
-    "purple"
-  ),
-];
+// export const contacts = [
+//   new Contact(
+//     "Ivo",
+//     "ivić",
+//     "1995-10-16",
+//     "156181554",
+//     "Moja Kuca 1",
+//     "Split",
+//     "red"
+//   ),
+//   new Contact(
+//     "Ante",
+//     "Lukač",
+//     "1989-03-23",
+//     "15132131554",
+//     "Moja Kuca 2",
+//     "Split",
+//     "green"
+//   ),
+//   new Contact(
+//     "Josip",
+//     "Horvat",
+//     "1968-08-30",
+//     "153231554",
+//     "Moja Kuca 3",
+//     "Zagreb",
+//     "green"
+//   ),
+//   new Contact(
+//     "Luka",
+//     "Horvat",
+//     "1993-04-05",
+//     "15313554",
+//     "Moja Kuca 4",
+//     "Osjek",
+//     "red"
+//   ),
+//   new Contact(
+//     "mate",
+//     "Horvat",
+//     "2001-03-07",
+//     "1561232554",
+//     "Moja Kuca 5",
+//     "Split",
+//     "purple"
+//   ),
+// ];
